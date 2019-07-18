94451eadbd9b5ef05240aef29fa0c5006459156f
Sieciech <wojciech.mocek@o2.pl>
2019-07-18 15:32:00 +0200

    added not synchronized projects

---
+++ package-test/golang-test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "fmt"
+    "time"
+    "github.com/Sieciechu/stringutil"
+)
+
+func readword(ch chan string) {
+    fmt.Println("Type a word, then hit Enter.")
+    var word string
+    fmt.Scanf("%s", &word)
+    ch <- word
+}
+
+func timeout(t chan bool) {
+    for i := 5; i > 0; i-- {
+        time.Sleep(1 * time.Second)
+        if i%2==0 {
+            fmt.Printf("%v seconds left\n", i)
+        }
+	}
+    t <- false
+}
+
+func main() {
+
+    fmt.Println(stringutil.Reverse("!oG, olleH"))
+
+
+
+    t := make(chan bool)
+    go timeout(t)
+
+    ch := make(chan string)
+    go readword(ch)
+
+    select {
+    case word := <-ch:
+        fmt.Println("Received", word)
+    case <-t:
+        fmt.Println("Timeout.")
+    }
+}
